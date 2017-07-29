5a93e78e404c244e51a26985f280aa50d737f4f3
Dillon Brout <djbrout@gmail.com>
2017-07-29 01:05:08 -0400

    getting ready to fit

--- mcmc_manager.py
+++ mcmc_manager.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
             , weights=inp['weights']
             , substamp=inp['substamp']
             , Nimage=inp['Nimage']
-            , maxiter = 45000 #, maxiter=inp['maxiter']
+            , maxiter = 95000 #, maxiter=inp['maxiter']
             , mask=inp['mask']
             , sky=inp['sky']
             , mjd=inp['mjd']
