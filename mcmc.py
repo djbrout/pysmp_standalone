881dcad6b9eb8e53a05e3a1ca9144b8494d7cb36
Dillon Brout <djbrout@gmail.com>
2017-08-03 10:52:26 -0400

    getting ready to fit

--- mcmc.py
+++ mcmc.py
@@ -742,7 +742,7 @@ class metropolis_hastings():
                 if (self.counter % 20000) == 0:
                     self.plotchains()
                     self.savechains()
-                    self.plotstamps()
+                    #self.plotstamps()
                 # sys.exit()
                 # self.plotstamps()
                     import gc
