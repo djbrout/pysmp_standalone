e6bf699dc51040ea720f20acebf8ce0465812683
Dillon Brout <djbrout@gmail.com>
2017-08-03 10:38:15 -0400

    getting ready to fit

--- mcmc.py
+++ mcmc.py
@@ -739,7 +739,7 @@ class metropolis_hastings():
                         # print 'fit galaxy registration', self.xgal_pix_offset[10:20]
                         # print 'fitting position:', self.x_pix_offset, self.y_pix_offset
 
-                if (self.counter % 10000) == 0:
+                if (self.counter % 20000) == 0:
                     self.plotchains()
                     self.savechains()
                     self.plotstamps()
