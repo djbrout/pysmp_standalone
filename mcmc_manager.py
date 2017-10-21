eb99998d89fea9df484d5aa5da9fe898d91fb650
Dillon Brout <djbrout@gmail.com>
2017-10-21 17:27:57 -0400

    redoing failed sne

--- mcmc_manager.py
+++ mcmc_manager.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
             , weights=inp['weights']
             , substamp=inp['substamp']
             , Nimage=inp['Nimage']
-            , maxiter = 2000000 #, maxiter=inp['maxiter']
+            , maxiter = 1000000 #, maxiter=inp['maxiter']
             , mask=inp['mask']
             , sky=inp['sky']
             , mjd=inp['mjd']
