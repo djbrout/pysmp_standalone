02894adc175f2925726f1b276cf5eb3b0a0505f0
Dillon Brout <djbrout@gmail.com>
2017-12-16 14:31:28 -0500

    more deep fields

--- mcmc.py
+++ mcmc.py
@@ -741,8 +741,7 @@ class metropolis_hastings():
 
             if (self.counter % 1000) == 0:
                 self.accepted_int = 0
-                print 'LOCAL GALAXY MEAN', np.mean(self.galmodel[14:17,14:17])
-                self.local_galchain.append(np.mean(self.galmodel[14:17,14:17]))
+                self.local_galchain.append(np.mean(self.galaxy_model[14:17,14:17]))
                 print 'Acceptance Rate:', self.accepted_history
                 if self.counter > 5000:
                     if float(self.accepted_history) < .25:
@@ -775,6 +774,7 @@ class metropolis_hastings():
                 # self.tps = tps
                 print 'Time per step:', tps
                 print 'PSF Position:', self.current_x_offset, self.current_y_offset
+                print 'LOCAL GALAXY MEAN', np.mean(self.galaxy_model[14:17,14:17])
                 print self.filt
                 #open('output/log.log', 'a').write(str(self.filt) + ' ' + str(self.counter) + ' TPS: ' + str(tps) + '\n')
                 # print 'mjdoff: ',self.mjdoff
