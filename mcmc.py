9dff0ccc07e3f2a69e4df3402c7784ff2ab247fe
Dillon Brout <djbrout@gmail.com>
2018-02-13 15:51:41 -0500

    getting ready for big run

--- mcmc.py
+++ mcmc.py
@@ -816,7 +816,7 @@ class metropolis_hastings():
                         # print 'fit galaxy registration', self.xgal_pix_offset[10:20]
                         # print 'fitting position:', self.x_pix_offset, self.y_pix_offset
 
-                if (self.counter % 50000) == 0:
+                if (self.counter % 1000) == 0:
                     self.plotchains()
                     self.savechains()
                     #self.plotstamps()
